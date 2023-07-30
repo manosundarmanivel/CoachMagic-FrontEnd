717720835915593bb183b3a44717a9c880b2cbf6
manosundarmanivel <manosundarmanivel@gmail.com>
2023-07-30 11:58:31 +0530

    if applied this commit will include testimonials with swiper

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,41 +1,215 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Navigation, Pagination,  A11y } from "swiper/modules";
 
 const Testimonials = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-sans font-bold text-center pb-20'>Here are our users <br/> Testimonials</h1>
+        <Swiper
+      modules={[Navigation, Pagination,  A11y]}
+      spaceBetween={10}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+     
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pl-10 pb-8">
+          <div className="w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg">
+            <div>
+              <div className="flex">
+                <img
+                  className="h-14 rounded-full"
+                  src="https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png"
+                />
+                <div className="pl-3">
+                  <h1 className="font-sans font-semibold">ChiChi</h1>
+                  <h1 className="font-sans font-light">South Africa</h1>
+                </div>
+              </div>
+            </div>
+            <div className="pt-3">
+              <h1 className="font-serif ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                consectetur justo quis euismod vehicula. Quisque diam dui,
+                imperdiet et
+              </h1>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      
+     
+    </Swiper>
+    </div>
     
-    return(
-        <div>
-            <h1 className='text-3xl font-sans font-bold text-center'>Here are our users <br/> testimonials</h1>
-            <div className='flex flex-wrap p-8'>
-            
-            {
-                 Array(6).fill("").map((index)=>
-                 {
-                     return (
-                         <div className='p-5'>
-                             
-                             <div className='w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg'>
-                                 <div>
-                                     <div className='flex'>
-                                         <img className= 'h-14 rounded-full' src='https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png'/>
-                                         <div className='pl-3'>
-                                         <h1 className='font-sans font-semibold'>ChiChi</h1>
-                                         <h1 className='font-sans font-light'>South Africa</h1>
-                                         </div>
-                                     </div>
-                                 </div>
-                                 <div className='pt-3'>
-                                     <h1 className='font-serif '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet et</h1>
-                                 </div>
-                             </div>
-                         </div>
-                       )
-                 })
-                 
-            }
-        </div>
-        </div>
-    )
-  
-}
+  );
 
-export default Testimonials
+  // return(
+  //     <div>
+  //         <h1 className='text-3xl font-sans font-bold text-center'>Here are our users <br/> testimonials</h1>
+  //         <div className='flex flex-wrap p-8'>
+
+  //         {
+  //              Array(6).fill("").map((index)=>
+  //              {
+  //                  return (
+  //  <div className='p-5'>
+
+  //      <div className='w-[400px] h-48 shadow-lg m-3 p-5 rounded-lg'>
+  //          <div>
+  //              <div className='flex'>
+  //                  <img className= 'h-14 rounded-full' src='https://www.mecgale.com/wp-content/uploads/2017/08/dummy-profile.png'/>
+  //                  <div className='pl-3'>
+  //                  <h1 className='font-sans font-semibold'>ChiChi</h1>
+  //                  <h1 className='font-sans font-light'>South Africa</h1>
+  //                  </div>
+  //              </div>
+  //          </div>
+  //          <div className='pt-3'>
+  //              <h1 className='font-serif '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet et</h1>
+  //          </div>
+  //      </div>
+  //  </div>
+  //                    )
+  //              })
+
+  //         }
+  //     </div>
+  //     </div>
+  // )
+};
+
+export default Testimonials;
