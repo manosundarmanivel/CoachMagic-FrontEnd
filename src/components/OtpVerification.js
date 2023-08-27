1bc643f2cb767e136f43f0a621ba6c363bf77e3a
KIRUTHIYAASHREE S P <98012434+kiruthiyaashree@users.noreply.github.com>
2023-08-27 22:34:08 +0530

    otp verification update

--- src/components/OtpVerification.js
+++ src/components/OtpVerification.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import OTPInput, { ResendOTP } from "otp-input-react";
-import OtpImg from '../img/OTP 1.svg'
+import Otp from '../img/EnterOTP.mp4'
 
 const OtpVerification = () => {
   const [OTP, setOTP] = useState(0);
   return (
     <div className="flex justify-center items-center m-40   font-poppins">
       <div className="text-center">
-        <div className="pl-3 m-5">
-        <img alt = 'img'src={OtpImg}/>
+        <div className="w-[50%]">
+        <video autoPlay  muted className="ml-[10rem]">
+                <source src={Otp} type='video/mp4'/>
+            </video>
         </div>
         <h1 className="text-[#5B5B5B] font-bold text-[24px]">OTP Verification</h1>
         <h1 className="pt-3">Enter the OTP sent to  <span className="text-black font-bold">+91 63xxxxxx74</span></h1>
-        <div className="pl-10 pt-5 text-center ">
+        <div className="pl-10 pt-5 text-center flex justify-center ">
           <OTPInput
             inputStyles={{
               borderColor: "gray",
@@ -32,9 +34,7 @@ const OtpVerification = () => {
         <div className="">
         <ResendOTP onResendClick={() => console.log("Resend clicked")} />
         </div>
-        <div className="bg-gradient-to-r m-auto from-cyan-500 to-blue-500 m-5 rounded-full">
-         <h1 className="text-white p-2 m-3 font-semibold ">Verify</h1>
-        </div>
+         <button className="text-white py-2 px-7 font-semibold w-min  bg-gradient-to-r from-cyan-500 to-blue-500  rounded-full">Verify</button>
         
       </div>
     </div>
